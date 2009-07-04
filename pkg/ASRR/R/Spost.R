8f0050a1b5b1e1859b103e0bb36fd404fea10015
wincent <wincent@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-07-04 01:34:04 +0000

    listcoef for probit model
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/asrr@26 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ASRR/R/Spost.R
+++ pkg/ASRR/R/Spost.R
@@ -1,41 +1,66 @@
 listcoef <- function(x,...) {
   UseMethod("listcoef")
-  }
+}
 
 listcoef.lm <- function(x,...){
-	##www.nd.edu/~rwilliam/stats1/x92b.pdf
-	## x is a lm model with x and y setted to TRUE.
-	SDofX <- apply(x$x,2,sd)
-	SDofY <- sd(x$y)
-	Coef <- summary(x)$coef
-	Est <- Coef[,"Estimate"]
-	bStdY <- Est/SDofY
-	bStdX <- Est*SDofX
-	bStdXY <- Est*SDofX/SDofY
-	ans <- cbind(Coef,bStdX=bStdX,bStdY=bStdY,bStdXY=bStdXY,SDofX=SDofX,SDofY=SDofY)
-	ans
-	}
-	
-listcoef.glm <- function(x,...){	
-	listcoef_logistic <- function(x,...){
-		## logistic model
-		## http://www.nd.edu/~rwilliam/xsoc73994/L03.pdf
-		## http://www.nd.edu/~rwilliam/zsoc694/x04.pdf
-		Xb <- predict(x)
-		SDofX <- apply(x$x,2,sd)
-		SDofY <- sd(x$y)
-		SDofYstar <- sqrt(var(Xb)+pi^2/3)
-		Coef <- summary(x)$coef
-		Est <- Coef[,"Estimate"]
-		bStdY <- Est/SDofYstar
-		bStdX <- Est*SDofX
-		bStdXY <- Est*SDofX/SDofYstar
-		ans <- cbind(Coef,bStdX=bStdX,bStdY=bStdY,bStdXY=bStdXY,SDofX=SDofX,SDofY=SDofY,"SDofY*"=SDofYstar)
-		ans
-		}
-	ans <- NULL
-	if (x$family$family == "binomial" && x$family$link == "logit") { ans <- listcoef_logistic(x) }
-	## if ...
-	ans
-	}
+  ##www.nd.edu/~rwilliam/stats1/x92b.pdf
+  ## x is a lm model with x and y setted to TRUE.
+  SDofX <- apply(x$x,2,sd)
+  SDofY <- sd(x$y)
+  Coef <- summary(x)$coef
+  Est <- Coef[,"Estimate"]
+  bStdY <- Est/SDofY
+  bStdX <- Est*SDofX
+  bStdXY <- Est*SDofX/SDofY
+  ans <- cbind(Coef,bStdX=bStdX,bStdY=bStdY,bStdXY=bStdXY,SDofX=SDofX,SDofY=SDofY)
+  ans
+}
 
+listcoef.glm <- function(x,...){
+  listcoef_logistic <- function(x,...){
+    ## logistic model
+    ## http://www.nd.edu/~rwilliam/xsoc73994/L03.pdf
+    ## http://www.nd.edu/~rwilliam/zsoc694/x04.pdf
+    Xb <- predict(x)
+    SDofX <- apply(x$x,2,sd)
+    SDofY <- sd(x$y)
+    SDofYstar <- sqrt(var(Xb)+pi^2/3)
+    Coef <- summary(x)$coef
+    Est <- Coef[,"Estimate"]
+    bStdY <- Est/SDofYstar
+    bStdX <- Est*SDofX
+    bStdXY <- Est*SDofX/SDofYstar
+    ans <- cbind(Coef,"e^b"=exp(Est),
+                 bStdX=bStdX,bStdY=bStdY,bStdXY=bStdXY,
+                 "e^bStdX"=exp(bStdX),"e^bStdY"=exp(bStdY),"e^bStdXY"=exp(bStdXY),
+                 SDofX=SDofX,SDofY=SDofY,"SDofY*"=SDofYstar)
+    ans
+  }
+  listcoef_probit <- function(x,...){
+    Xb <- predict(x)
+    SDofX <- apply(x$x,2,sd)
+    SDofY <- sd(x$y)
+    SDofYstar <- sqrt(var(Xb)+1)
+    Coef <- summary(x)$coef
+    Est <- Coef[,"Estimate"]
+    bStdY <- Est/SDofYstar
+    bStdX <- Est*SDofX
+    bStdXY <- Est*SDofX/SDofYstar
+    ans <- cbind(Coef, bStdX=bStdX,bStdY=bStdY,bStdXY=bStdXY,
+                 SDofX=SDofX,SDofY=SDofY,"SDofY*"=SDofYstar)
+    ans
+  }
+  listcoef_poisson <- function(x,...){
+    SDofX <- apply(x$x,2,sd)
+    Coef <- summary(x)$coef
+    Est <- Coef[,"Estimate"]
+    bStdX <- Est*SDofX
+    ans <- cbind(Coef,bStdX=bStdX,"e^b"=exp(Est),"e^bStdX"=exp(bStdX),
+                "%"= (exp(Est)-1)*100,"%StdX"= (exp(bStdX)-1)*100,SDofX=SDofX)
+  }
+  ans <- NULL
+  if (x$family$family == "binomial" && x$family$link == "logit") { ans <- listcoef_logistic(x) }
+  if (x$family$family == "binomial" && x$family$link == "probit") { ans <- listcoef_probit(x) }
+  if (x$family$family == "poisson" && x$family$link == "log") { ans <- listcoef_poisson(x) }
+  ans
+}
