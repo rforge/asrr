b9deb8d3b28e952bb9239b174d7ceba878640dec
wincent <wincent@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-25 02:07:26 +0000

    new function of associations.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/asrr@77 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ASRR/R/associations.R
+++ pkg/ASRR/R/associations.R
@@ -1,13 +1,24 @@
+## part of the ASRR package.
+## R/associations.R
+## By Ronggui HUANG 25-03-2010
+
 associations <- function(x,y){
+## x, y are vectors, passed to table
+## This function packs codes to make easy call to CoCo::partialAssociations
     require(CoCo)
+    xname <- substitute(x)
+    xname <- gsub("[$]", "_", deparse(xname))
+    yname <- substitute(y)
+    yname <- gsub("[$]", "_", deparse(yname))
     tab <- table(x, y)
     ## create a contingency table
-    tryCatch(CoCo::endCoCo(),error=function(e)invisible())
+    names(dimnames(tab)) <- c(xname,yname)
+    tryCatch(CoCo::endCoCo(object=cc),error=function(e) invisible())
     ## end CoCo object if there is any
     cc <- CoCo::makeCoCo() ##  create a CoCo object
     CoCo::enterTable(tab, object = cc) ## add contingency table to CoCo object
     ## show variables in the CoCo object
-    CoCo::partialAssociations(CoCo::returnVariableDescription(),object = cc)
+    CoCo::partialAssociations(CoCo::returnVariableDescription(object=cc)$names,object = cc)
     ## return associations
-    CoCo::endCoCo()
+    CoCo::endCoCo(object=cc,silent=TRUE)
 }
