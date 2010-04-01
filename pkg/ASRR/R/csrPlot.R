81b5ef84627688edd835f6525a7fd952e5ab7af0
wincent <wincent@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-04-01 16:03:30 +0000

    new function of csrPlot
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/asrr@80 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/ASRR/R/csrPlot.R
@@ -0,0 +1,9 @@
+csrPlot <- function(x,...){
+  ## x is a coxph model fitted by survial::coxph
+  martingale <- residuals(x,type="martingale")
+  coxSnellResidual <- 1- martingale
+  ## Tableman and Kim (2003) about relationship between two types of residuals
+  plot(survfit(Surv(coxSnellResidual,x$y[,'status'])~1),fun="cumhaz",firstx=0, 
+       xlab="cox-snell residuals",ylab="H(t) based on cox-snell residuals")
+  abline(a=0,b=1, lty=3)
+}
