f3843ce73265839a8a3f06b14ba691d83658c195
wincent <wincent@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-04-19 04:09:30 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/asrr@82 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ASRR/R/csrPlot.R
+++ pkg/ASRR/R/csrPlot.R
@@ -4,8 +4,8 @@ csrPlot <- function(x,...){
   require(survival)
   martingale <- residuals(x,type="martingale")
   coxSnellResidual <- 1- martingale
-  ## Tableman and Kim (2003) about relationship between two types of residuals
-  plot(survfit(Surv(coxSnellResidual,x$y[,'status'])~1),fun="cumhaz",firstx=0, 
+  ## Tableman and Kim (2003:160) about relationship between two types of residuals
+  plot(survfit(Surv(coxSnellResidual,x$y[,'status'])~1),fun="cumhaz",firstx=0,
        xlab="cox-snell residuals",ylab="H(t) based on cox-snell residuals")
   abline(a=0,b=1, lty=3)
 }
