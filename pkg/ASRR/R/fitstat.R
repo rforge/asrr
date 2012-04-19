a7deaa4843ffca3996188de37312fa8524c30fe5
wincent <wincent@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-04-19 12:38:03 +0000

    fitstat.glm does all indices for binary model (Long,1997)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/asrr@153 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ASRR/R/fitstat.R
+++ pkg/ASRR/R/fitstat.R
@@ -2,6 +2,7 @@ fitstat.glm <- function(x, ...) {
     na <- na.action(x)
     if (is.null(na)) x.base <- update(x,.~1)
     else x.base <- update(x,.~1,subset=-na.action(x))
+    LRT <- anova(x.base, x, test="LRT")
     if (x$family$family == "binomial" && (x$family$link %in% c("logit","probit"))){
         y <- x$y
         pr <- predict(x,type="response")
@@ -19,19 +20,26 @@ fitstat.glm <- function(x, ...) {
         Count.R2 <- sum(diag(tab))/sum(tab)
         Count.Adj.R2 <- (sum(diag(tab))-maxrow)/(sum(tab)-maxrow)
         L.full <- logLik(x)
+        P <- attr(L.full,"df")
         L.base <- logLik(x.base)
         Deviance=deviance(x)
         if (attr(L.full,"n")!= attr(L.base,"n")) stop("The number of obs in the intercept only model differs.")
         N <- attr(L.full,"n")
-        K <- attr(L.full,"df")
         attributes(L.full) <- attributes(L.base) <- NULL
         McFadden.R2 <- 1-(L.full/L.base)
-        McFadden.Adj.R2 <- 1-((L.full-K)/L.base)
+        McFadden.Adj.R2 <- 1-((L.full-P)/L.base)
         ML.R2 <- 1 - exp(2*(L.base-L.full)/N) ## AKA. Cox-Snell
         Cragg.Uhler.R2 <- ML.R2/(1-exp(2*L.base/N)) ## AKA Nagelkerke
-        ans <- list(Count.R2=Count.R2, Count.Adj.R2=Count.Adj.R2,Efron.R2=Efron.R2, M.Z.R2=M.Z.R2,
+        AIC <- (-2*L.full+2*P)/N ## definition of Long(1997:109)
+        AICplusN <- AIC*N
+        BIC1 <- Deviance - (N-P)*log(N)
+        BIC2 <- -2*(L.full-L.base) + (P-1)*log(N) ## P-1 is number of IVs
+        ans <- list(LL=L.full,LL0=L.base,Deviance=Deviance, LRT=LRT,
+                    Count.R2=Count.R2, Count.Adj.R2=Count.Adj.R2,
+                    Efron.R2=Efron.R2, M.Z.R2=M.Z.R2,
                     McFadden.R2=McFadden.R2,McFadden.Adj.R2=McFadden.Adj.R2,
-                    ML.R2=ML.R2,Cragg.Uhler.R2=Cragg.Uhler.R2,Deviance=Deviance)
+                    ML.R2=ML.R2,Cragg.Uhler.R2=Cragg.Uhler.R2,
+                    AIC=AIC, AICplusN=AICplusN, BIC1=BIC1, BIC2=BIC2)
     ans
     }
 }
