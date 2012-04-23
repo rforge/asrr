265b1af532e794c0ea44ba798e2df44efc05ad6c
wincent <wincent@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-04-23 12:18:40 +0000

    fitstat.polr
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/asrr@154 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ASRR/R/fitstat.R
+++ pkg/ASRR/R/fitstat.R
@@ -1,3 +1,5 @@
+## need improvement when weight is not NULL, esp. N changes
+
 fitstat.glm <- function(x, ...) {
     na <- na.action(x)
     if (is.null(na)) x.base <- update(x,.~1)
@@ -40,6 +42,53 @@ fitstat.glm <- function(x, ...) {
                     McFadden.R2=McFadden.R2,McFadden.Adj.R2=McFadden.Adj.R2,
                     ML.R2=ML.R2,Cragg.Uhler.R2=Cragg.Uhler.R2,
                     AIC=AIC, AICplusN=AICplusN, BIC1=BIC1, BIC2=BIC2)
-    ans
+        class(ans) <- "fitstat"
+        ans
     }
 }
+
+fitstat.polr <- function(x,...){
+    na <- na.action(x)
+    if (is.null(na)) x.base <- update(x,.~1)
+    else x.base <- update(x,.~1,subset=-na.action(x))
+    LRT <- anova(x.base, x, test="Chisq")
+    if (is.null(x$model)) x <- update(x, model=TRUE)
+    yhat <- predict(x)
+    y <- model.response(x$model)
+    tab <- table(y, yhat)
+    maxrow <- max(rowSums(tab))
+    Count.R2 <- sum(diag(tab))/sum(tab)
+    Count.Adj.R2 <- (sum(diag(tab))-maxrow)/(sum(tab)-maxrow)
+    L.full <- logLik(x)
+    P <- attr(L.full,"df")
+    L.base <- logLik(x.base)
+    Deviance=deviance(x)
+    if (attr(L.full,"n")!= attr(L.base,"n")) {
+        stop("The number of obs in the intercept only model differs.")
+    }
+    N <- attr(L.full,"n")
+    attributes(L.full) <- attributes(L.base) <- NULL
+    McFadden.R2 <- 1-(L.full/L.base)
+    McFadden.Adj.R2 <- 1-((L.full-P)/L.base)
+    ML.R2 <- 1 - exp(2*(L.base-L.full)/N) ## AKA. Cox-Snell
+    Cragg.Uhler.R2 <- ML.R2/(1-exp(2*L.base/N)) ## AKA Nagelkerke
+    AIC <- (-2*L.full+2*P)/N ## definition of Long(1997:109)
+    AICplusN <- AIC*N
+    BIC1 <- Deviance - (N-P)*log(N)
+    BIC2 <- -2*(L.full-L.base) + (P-1)*log(N) ## P-1 is number of IVs
+    Vystar <- var(x$lp)
+    M.Z.R2 <-  if (x$method == "logistic"){
+        Vystar/(Vystar+pi^2/3)
+    } else  if (x$method == "probit") {
+        Vystar/(Vystar+1)
+    } else {NA}
+    ans <- list(LL=L.full,LL0=L.base,Deviance=Deviance, LRT=LRT,
+                Count.R2=Count.R2, Count.Adj.R2=Count.Adj.R2,
+                Efron.R2=NA, M.Z.R2=M.Z.R2,
+                McFadden.R2=McFadden.R2,McFadden.Adj.R2=McFadden.Adj.R2,
+                ML.R2=ML.R2,Cragg.Uhler.R2=Cragg.Uhler.R2,
+                AIC=AIC, AICplusN=AICplusN, BIC1=BIC1, BIC2=BIC2)
+    class(ans) <- "fitstat"
+    ans
+}
+
