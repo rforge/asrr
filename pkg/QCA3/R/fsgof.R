cea41e89b46c1afbb44841c7b18ee686d73209b6
wincent <wincent@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-13 08:28:01 +0000

    new function of gof.descr
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/asrr@159 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/QCA3/R/fsgof.R
+++ pkg/QCA3/R/fsgof.R
@@ -14,12 +14,13 @@ Dnull <- function(y, damp=.01){
 	yZ <- zTransform(y,damp=damp)
 	df <- length(y) - 1
 	ssd <- sum((yZ -mean(yZ))^2)
-      msd <- ssd/df
+	msd <- ssd/df
 	ans <- list(ssd=ssd,df=df,msd=msd)
 	ans
 }
 
 fsgof.nec <- function(y, x, damp=.01, error=.05) {
+	dn <- sprintf("%s and %s", deparse(substitute(y)),deparse(substitute(x)))
 	yZ <- zTransform(y,damp=damp)
 	xZ <- zTransform(x,damp=damp)
 	d <- as.numeric(y > x)
@@ -36,12 +37,13 @@ fsgof.nec <- function(y, x, damp=.01, error=.05) {
 	attr(F,"names") <- "F"
 	pval <- pf(F,df1, df2, lower=FALSE)
 	ans <- list(estimate=estimate,statistic=F,parameter=para,p.value=pval,
-	method="Test of Causual Necessity.",data.name="y and x")
+	method="Test of Causual Necessity.",data.name=dn)
 	class(ans) <- "htest"
 	ans
 }
 
 fsgof.suff <- function(y, x, damp=.01, error=.05) {
+	dn <- sprintf("%s and %s", deparse(substitute(y)),deparse(substitute(x)))
 	xZ <- zTransform(x,damp=damp)
 	yZ <- zTransform(y,damp=damp)
 	d <- as.numeric(y > x)
@@ -59,12 +61,13 @@ fsgof.suff <- function(y, x, damp=.01, error=.05) {
 	pval <- pf(F,df1, df2, lower=FALSE)
 	ans <- list(estimate=estimate,
 	statistic=F,parameter=para,p.value=pval,
-	method="Test of Causual Sufficiency.",data.name="y and x")
+	method="Test of Causual Sufficiency.",data.name=dn)
 	class(ans) <- "htest"
 	ans
 }
 
 fsgof.suffnec <- function(y, x, damp=.01, error=.05) {
+	dn <- sprintf("%s and %s", deparse(substitute(y)),deparse(substitute(x)))
 	xZ <- zTransform(x,damp=damp)
 	yZ <- zTransform(y,damp=damp)
 	d <- as.numeric(y > x)
@@ -81,12 +84,47 @@ fsgof.suffnec <- function(y, x, damp=.01, error=.05) {
 	pval <- pf(F,df1, df2, lower=FALSE)
 	ans <- list(estimate=estimate,
 	statistic=F,parameter=para,p.value=pval,
-	method="Test of Causual Sufficiency and Necessity.",data.name="y and x")
+	method="Test of Causual Sufficiency and Necessity.",data.name=dn)
 	class(ans) <- "htest"
 	ans
 }
 
-##y=c(0.8,0.5,0.5,0.4); x=c(0.7,0.3,0.4,0.45)
+fsgof.test <- function(y, x, type=c("suffnec","suff","nec"), damp=.01, error=.05){
+	type <- match.arg(type)
+	ans <- switch(type,
+		"suffnec"=fsgof.suffnec(y, x, damp=damp, error=error),
+ 		"nec"=fsgof.nec(y, x, damp=damp, error=error),
+		"suff"=fsgof.suff(y, x, damp=damp, error=error)
+	)
+	ans
+}
+
+fsgof.descr <- function(y, x, damp=.01){
+	yZ <- zTransform(y,damp=damp)
+	xZ <- zTransform(x,damp=damp)
+	d <- as.numeric(y > x)
+	Dnull <- sum((yZ -mean(yZ))^2)
+	Dnec <- sum(d*(yZ - xZ)^2)
+	Dsuff <- sum((1-d)*(yZ - xZ)^2)
+	RDnull <- Dnull/(Dnull+Dnec+Dsuff)
+	RDnec <- Dnec/(Dnull+Dnec+Dsuff)
+	RDsuff <- Dsuff/(Dnull+Dnec+Dsuff)
+	RDsuffnec <- (Dnec+Dsuff)/(Dnull+Dnec+Dsuff)
+	Rnull <- 1-RDnull
+	Rnec<- 1-RDnec
+	Rsuff <- 1-RDsuff
+	Rsuffnec <- 1-RDsuffnec
+	relative.distance <- data.frame(null=RDnull,nec=RDnec,suffnec=RDsuffnec)
+	relative.consistency <- data.frame(null=Rnull,nec=Rnec,suffnec=Rsuffnec)
+	ans <- rbind(relative.distance,relative.consistency)
+	row.names(ans) <- c("distance","consistency")
+	ans
+}
+
+##y=c(0.8,0.5,0.5,0.4); x=c(0.7,0.3,0.4,0.45);x2 =c(0.66,0.55,0.22,0.52)
 #fsgof.nec(y,x)
 #fsgof.suff(y,x)
 #fsgof.suffnec(y,x)
+#fsgof.descr(y,x)
+#high order test
+#fsgof.suffnec(y,pmin(x,x2))
